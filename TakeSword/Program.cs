ac8ed9555ff7fe5978b8958971eb8e5fee34f994
Robert Davidson <rdavidson@accusoft.com>
2022-01-07 14:51:42 -0500

    kill dead code.

--- TakeSword/Program.cs
+++ TakeSword/Program.cs
@@ -69,9 +69,6 @@ namespace TakeSword
     {
         public static void Main(string[] args)
         {
-            // Verbs
-            var verbSuite = new VerbSuite<Entity>(VerbUtil.GenerateVerbs());
-
             World world = new();
             WorldSetup.Apply(world, out Entity playerEntity, out Entity startLocation);
             OutputEntry description = DescriptionUtilities.GetDescription(startLocation, playerEntity);
@@ -80,81 +77,6 @@ namespace TakeSword
             while (true)
             {
                 world.Run();
-            }
-
-            while (true)
-            {
-                Console.Write(">");
-                string input = Console.ReadLine()!;
-                ActionOutcome? backupFailure = null;
-                IGameAction? validAction = null;
-                foreach (IGameAction action in verbSuite.GetMatches(playerEntity, input))
-                {
-                    ActionOutcome testOutcome = action.Execute(dryRun: true);
-                    if (testOutcome.Status == Failed)
-                    {
-                        if (backupFailure == null)
-                        {
-                            backupFailure = testOutcome;
-                        }
-                    }
-                    else
-                    {
-                        validAction = action;
-                    }
-                }
-
-                if (validAction is null)
-                {
-                    if (string.IsNullOrEmpty(backupFailure?.Message))
-                    {
-                        Console.WriteLine("Invalid: I couldn't recognize the verb of that sentence.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Invalid: {backupFailure.Message}");
-                    }
-
-                    continue;
-                }
-
-                ActionOutcome outcome = validAction.Execute();
-                while (outcome.Status == InProgress)
-                {
-                    if (string.IsNullOrEmpty(outcome.Message))
-                    {
-                        Console.WriteLine("...");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"({outcome.Message})...");
-                    }
-                    outcome = validAction.Execute();
-                }
-
-                if (outcome.Status == Failed)
-                {
-                    if (string.IsNullOrEmpty(outcome.Message))
-                    {
-                        Console.WriteLine("Failed.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Failed: {outcome.Message}");
-                    }
-                }
-                else
-                {
-                    if (string.IsNullOrEmpty(outcome.Message))
-                    {
-                        Console.WriteLine("Done!");
-                    }
-                    else
-                    {
-                        Console.WriteLine(outcome.Message);
-                    }
-                }
-                
             }
         }
     }
