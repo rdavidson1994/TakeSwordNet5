bd7acab363a32e8db7abd63e31e15e567b5602f3
Robert Davidson <rdavidson@accusoft.com>
2022-01-09 14:16:36 -0700

    Expand the HitAction to support weapons

--- TakeSword/Actions/HitAction.cs
+++ TakeSword/Actions/HitAction.cs
@@ -2,28 +2,46 @@
 
 namespace TakeSword
 {
-    public record HitAction(Entity Actor, Entity Victim) : IGameAction
+    public record HitAction(Entity Actor, Entity Victim, Entity? Weapon = null) : IGameAction
     {
         public ActionOutcome Execute(bool dryRun = false)
         {
             if (!Location.DoesMatch(Actor, Victim))
                 return Failure("the target is not present");
 
-            var attackAbility = Actor.Get<NaturalAttack>();
-            if (attackAbility == null)
-                return Failure("you have no natural methods of attacking.");
+            // Determine effective weapon traits
+            WeaponTraits attackWeaponTraits;
+            if (Weapon is not null)
+            {
+                // If a weapon is used, get it's traits directly
+                var weaponTraits = Weapon.Get<WeaponTraits>();
+                if (weaponTraits == null)
+                    // Fail if the selected weapon doesn't have weapon traits
+                    return Failure("that is not a suitable weapon.");
+                attackWeaponTraits = weaponTraits;
+            }
+            else
+            {
+                // Otherwise, use weapon traits from the actor's natural weapon
+                var attackAbility = Actor.Get<NaturalAttack>();
+                if (attackAbility == null)
+                    // Fail without it
+                    return Failure("you have no natural methods of attacking.");
+
+                (attackWeaponTraits, _) = attackAbility;
+            }
 
             // End of dry run
             if (dryRun)
                 return Success();
 
-            (WeaponTraits attackTraits, _) = attackAbility;
-            (int damage, _) = attackTraits;
+
+
+            (int damage, _) = attackWeaponTraits;
             var health = Victim.Get<Health>();
 
             if (health == null)
                 return Failure("the target takes no damange from your attack");
-            
 
             Victim.Set(health with { Amount = health - damage });
             return Success($"the victim takes {damage} damage. New health is {health - damage}");
